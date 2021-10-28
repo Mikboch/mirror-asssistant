ba01ad023a98247fa324b0c72aa9021de7ba191d
Mikboch <mikolaj.bochinski@gmail.com>
2021-10-29 00:53:05 +0200

    added new sounds and folder for storing them

--- picovoice.py
+++ picovoice.py
@@ -16,25 +16,18 @@ from pydub.playback import play
 import online_recognizer
 
 # print(pvporcupine.KEYWORDS)
+path_to_base_folder = "/home/pi/Desktop/Scripts/VoiceAssistant/"
+path_to_sounds_folder = "/home/pi/Desktop/Scripts/VoiceAssistant/sound_files/"
+
+def create_mp3_file_from_text(text, file_format):
+    tts = gTTS(text=text, lang="en", tld='ca')
+    tts.save(path_to_sounds_folder+text+file_format)
 
 
-handle = pvporcupine.create(keywords = ['picovoice', 'alexa'])
-
-pa = pyaudio.PyAudio()
-
-audio_stream = pa.open(
-                rate=handle.sample_rate,
-                channels=1,
-                format=pyaudio.paInt16,
-                input=True,
-                frames_per_buffer=handle.frame_length)
-
-def speak_from_saved_file(text):
-    # tts = gTTS(text=text, lang="en")
-    filename = "request.mp3"
-
-    # tts.save(filename)
+def speak_from_saved_file(name_of_file):
+    filename = path_to_sounds_folder+name_of_file
     playsound.playsound(filename)
+
 
 def speak(text):
     tts = gTTS(text=text, lang="en", tld='ca')
@@ -42,11 +35,9 @@ def speak(text):
     tts.write_to_fp(f)
     f.seek(0)
 
-
     transcribed_sentence = AudioSegment.from_file(f, format="mp3")
     play(transcribed_sentence)
     
-
     # mp3 = Mpg123()
     # mp3.feed(f.read())
     # out = Out123()
@@ -55,12 +46,25 @@ def speak(text):
     #     out.play(frame)
 
 
-
-speak("Hi I'm your alexa, what can i do for you?")
+#przetestowac czy ktoras z tych 3 linii nie produkuje false positivow -> przesunac ponizej speak()
+handle = pvporcupine.create(keywords = ['picovoice', 'alexa'])
+pa = pyaudio.PyAudio()
+audio_stream = pa.open(
+                rate=handle.sample_rate,
+                channels=1,
+                format=pyaudio.paInt16,
+                input=True,
+                frames_per_buffer=handle.frame_length)
+speak_from_saved_file("notification_ambient.wav")
+speak_from_saved_file("starting_voice.mp3")
+# create_mp3_file_from_text("Hi I'm your alexa, what can i do for you?","mp3")
 # speak_from_saved_file("Today happened: - In Sudan, a military coup deposes the government under Prime Minister Abdalla Hamdok. Ahead of Barbados becoming a republic, Sandra Mason (pictured) is elected as the country's first president.")
 # onlineRecognizer = online_recognizer.OnlineRecognizer
 
-time.sleep(5)
+# time.sleep(5)
+print("Elko")
+
+
 # main loop
 while True:
     pcm = audio_stream.read(handle.frame_length)
@@ -70,39 +74,10 @@ while True:
     if keyword_index >= 0:
         # detection event logic/callback
         print("Hey google")
+        speak_from_saved_file("state-change_confirm-down.wav")
         speak("To your service!")
-        online_recognizer.listen_for_command()
+        #online_recognizer.listen_for_command()
 
         
-       
-        
 
-
-# handle.delete()
-
-
-# from picovoice import Picovoice
- 
-# keyword_path = ...
- 
-# def wake_word_callback():
-#     pass
- 
-# context_path = ...
- 
-# def inference_callback(inference):
-#     # `inference` exposes three immutable fields:
-#     # (1) `is_understood`
-#     # (2) `intent`
-#     # (3) `slots`
-#     pass
- 
-# handle = Picovoice(
-#         keyword_path=keyword_path,
-#         wake_word_callback=wake_word_callback,
-#         context_path=context_path,
-#         inference_callback=inference_callback)
-        
-# while True:
-#     handle.process(get_next_audio_frame())
-# # handle.delete()
+# handle.delete()       <-this might be useful for echo effect
