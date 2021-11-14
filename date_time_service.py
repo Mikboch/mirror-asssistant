111433d7648e721de79149e11ce9b4ce170bf584
Mikboch <mikolaj.bochinski@gmail.com>
2021-11-15 00:46:10 +0100

    Date_time service almost done. Must perform tests!

--- date_time_service.py
+++ date_time_service.py
@@ -1,34 +1,51 @@
+import re
 import time
-import datetime 
+import datetime
 import requests
 import json
 
+# from datetime import datetime, date
+import calendar
 
-with open('/home/pi/Desktop/Scripts/VoiceAssistant/resources/api_keys.json') as f1:
+with open("/home/pi/Desktop/Scripts/VoiceAssistant/resources/api_keys.json") as f1:
     api_keys_file = json.load(f1)
     ip_geolocation_key = api_keys_file["ip_geolocation_key"]
 
-with open('/home/pi/Desktop/Scripts/VoiceAssistant/resources/intents/date_time_keywords.json') as f2:
+with open(
+    "/home/pi/Desktop/Scripts/VoiceAssistant/resources/intents/date_time_keywords.json"
+) as f2:
     keywords_file = json.load(f2)
-        
+
 
 # "What time is it in Warsaw"
 
+
 def take_command_and_return_info(command):
-    print("jestem tutaj")
+    print("Command taken, entering function")
+
     if "time" in command and "in" in command:
         distinct_words = filter_command(command)
         print(distinct_words)
         city = distinct_words[0]
-        if(len(distinct_words)>1):
+        if len(distinct_words) > 1:
             country = distinct_words[1]
-        time = get_time_in_city(city)#,distinct_words[1])
+        time = get_time_in_city(city)  # ,distinct_words[1])
         print(time)
-        return "In " +city+ " it is " + time
+
+        return "In " + city + " it is " + time
     elif "time" in command:
-        return datetime.datetime.now()
-    elif "":  
-        pass  
+
+        return "It's currently " + datetime.datetime.now()
+    elif "day" in command and "is" in command:
+        distinct_words = filter_command(command)
+        day = get_day_of_week(distinct_words)
+
+        return day
+    elif "date" in command:
+        distinct_words = filter_command(command)
+        date = get_date()
+
+        return date
 
 
 def filter_command(command):
@@ -44,10 +61,15 @@ def filter_command(command):
 
 
 def get_time_in_city(city):
-    request = 'https://api.ipgeolocation.io/timezone?apiKey='+ip_geolocation_key+'&location=' + city
+    request = (
+        "https://api.ipgeolocation.io/timezone?apiKey="
+        + ip_geolocation_key
+        + "&location="
+        + city
+    )
     response = requests.get(request)
-    
-    if(response.status_code == 200):
+
+    if response.status_code == 200:
         date_unprocessed = response.json()["date_time_txt"]
         day = date_unprocessed.split(",")[0]
 
@@ -56,4 +78,32 @@ def get_time_in_city(city):
         str2 = hours_unprocessed[-3:]
         hours = str1 + str2
 
-        return day+", "+ hours
+        return day + ", " + hours
+
+
+def get_date():
+    current_date = datetime.date.today()
+
+    day_of_week = current_date.strftime("%A")
+    date = current_date.strftime("%B %d %Y")
+
+    return "It's " + day_of_week + ", " + date
+
+
+def get_day_of_week(when):
+    WEEK_DAYS = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+
+    if "today" in when:
+        day_index = int(datetime.date.today().strftime("%w"))
+        return "Today is " + WEEK_DAYS[day_index]
+    elif "tomorrow" in when:
+        day_index = int(datetime.date.today().strftime("%w")) + 1
+        return "Tomorrow is " + WEEK_DAYS[day_index]
--- picovoice.py
+++ picovoice.py
@@ -21,41 +21,42 @@ path_to_sounds_folder = "./resources/sound_files/"
 
 
 def create_mp3_file_from_text(text, file_format):
-    tts = gTTS(text=text, lang="en", tld='ca')
-    tts.save(path_to_sounds_folder+text+file_format)
+    tts = gTTS(text=text, lang="en", tld="ca")
+    tts.save(path_to_sounds_folder + text + file_format)
 
 
 def speak_from_saved_file(name_of_file):
-    filename = path_to_sounds_folder+name_of_file
+    filename = path_to_sounds_folder + name_of_file
     playsound.playsound(filename)
 
 
 def speak(text):
-    tts = gTTS(text=text, lang="en", tld='ca')
+    tts = gTTS(text=text, lang="en", tld="ca")
     f = BytesIO()
     tts.write_to_fp(f)
     f.seek(0)
 
     transcribed_sentence = AudioSegment.from_file(f, format="mp3")
     play(transcribed_sentence)
-    
+
     # mp3 = Mpg123()
     # mp3.feed(f.read())
     # out = Out123()
-    
+
     # for frame in mp3.iter_frames(out.start):
     #     out.play(frame)
 
 
-#przetestowac czy ktoras z tych 3 linii nie produkuje false positivow -> przesunac ponizej speak()
-handle = pvporcupine.create(keywords = ['picovoice', 'alexa'])
+# przetestowac czy ktoras z tych 3 linii nie produkuje false positivow -> przesunac ponizej speak()
+handle = pvporcupine.create(keywords=["picovoice", "alexa"])
 pa = pyaudio.PyAudio()
 audio_stream = pa.open(
-                rate=handle.sample_rate,
-                channels=1,
-                format=pyaudio.paInt16,
-                input=True,
-                frames_per_buffer=handle.frame_length)
+    rate=handle.sample_rate,
+    channels=1,
+    format=pyaudio.paInt16,
+    input=True,
+    frames_per_buffer=handle.frame_length,
+)
 
 speak_from_saved_file("notification_ambient.wav")
 # speak_from_saved_file("starting_voice.mp3")
@@ -65,27 +66,28 @@ speak_from_saved_file("notification_ambient.wav")
 # onlineRecognizer = online_recognizer.OnlineRecognizer
 # time.sleep(5)
 
+command = "What day is it tomorrow "
 
+sentence = intent_manager.process_command(command)
+print(sentence)
 
-    
 
 # main loop
-while True:
-    pcm = audio_stream.read(handle.frame_length)
-    pcm = struct.unpack_from("h" * handle.frame_length, pcm)
+# while True:
+#     pcm = audio_stream.read(handle.frame_length)
+#     pcm = struct.unpack_from("h" * handle.frame_length, pcm)
 
-    keyword_index = handle.process(pcm)
-    if keyword_index >= 0:
-        # detection event logic/callback
-        print("Hey google")
-        speak("To your service!")
-        speak_from_saved_file("state-change_confirm-down.wav")
-        
-        command = online_recognizer.listen_for_command()
-        
-        sentence = intent_manager.process_command(command)
-        speak(sentence)
+#     keyword_index = handle.process(pcm)
+#     if keyword_index >= 0:
+#         # detection event logic/callback
+#         print("Hey google")
+#         speak("To your service!")
+#         speak_from_saved_file("state-change_confirm-down.wav")
 
-        
+#         command = online_recognizer.listen_for_command()
+
+#         sentence = intent_manager.process_command(command)
+#         speak(sentence)
+
 
 # handle.delete()       <-this might be useful for echo effect
